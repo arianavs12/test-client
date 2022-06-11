096fab0ccbb7addc61d074a8d741ed8aee56b8ed
Ariana <pilarsv@outlook.com>
2022-06-11 15:56:46 -0500

    :boom:

--- src/components/User.js
+++ src/components/User.js
@@ -130,9 +130,19 @@ function User() {
                     <h4>{datos.email}</h4>
                     <h4>{datos.phone}</h4>
                 </div>
-                <Button style={{margin: 8}} variant="primary" type='submit'>Iniciar</Button> 
+                <Button style={{margin: 8}} variant="primary" type='submit'>Iniciar</Button>
+                <div className='card'>
+                    <h4>{datos.nombre}</h4>
+                    <h4>{datos.secondName}</h4>
+                    <h4>{datos.lastName}</h4>
+                    <h4>{datos.secondLastName}</h4>
+                    <h4>{datos.day}</h4>
+                    <h4>{datos.month}</h4>
+                    <h4>{datos.year}</h4>
+                    <h4>{datos.email}</h4>
+                    <h4>{datos.phone}</h4>
+                </div> 
             </div>
-            
         </Form>
     </div>
 </Fragment>
